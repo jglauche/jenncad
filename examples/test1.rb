509c2170912c2f3ff13ba7e46a0bd39f25f6d890
Jennifer Glauche <mail@jglauche.de>
2019-05-05 19:40:14 +0200

    z-fighting issue example

--- examples/test1.rb
+++ examples/test1.rb
@@ -8,15 +8,13 @@ h1 = 10
 h2 = 20
 res = cube([10,20,h1]).center_xy
 res += cylinder(d:40,h:h2).move(z:h1)
-#res -= cylinder(d:10,h:5)
-#res -= cylinder(d:10,h:5).translate(z:15)
 
 
 n = Nut.new(4)
 res -= n.cut
 
 n = Nut.new(4)
-res -= n.cut.translate(z:h1+h2-n.height)
+res -= n.cut.move(z:h1+h2-n.height)
 
 
 
