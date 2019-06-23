956aada6d6c4c8aaef24c3f047730b9d27cc0f89
Jennifer Glauche <mail@jglauche.de>
2019-06-23 13:09:59 +0200

    make z-ref for all boolean objects

--- lib/jenncad/primitives/boolean_object.rb
+++ lib/jenncad/primitives/boolean_object.rb
@@ -18,6 +18,7 @@ module JennCad::Primitives
     def after_add
       @parts.flatten!
       @parts.compact!
+      @z = @parts.map(&:z).compact.max
       inherit_zref
     end
 
--- lib/jenncad/primitives/union_object.rb
+++ lib/jenncad/primitives/union_object.rb
@@ -1,10 +1,4 @@
 module JennCad::Primitives
   class UnionObject < BooleanObject
-    def after_add
-      @parts.flatten!
-      @parts.compact!
-      @z = @parts.map(&:z).compact.max
-      inherit_zref
-    end
   end
 end
